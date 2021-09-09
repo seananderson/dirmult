10421567d0cb8e98ceda69e918489f9796137267
Sean Anderson <sean@seananderson.ca>
2021-09-08 19:40:46 -0700

    Add linear

--- dirichlet.R
+++ dirichlet.R
@@ -1,5 +1,12 @@
 rdirichlet <- function(p, phi) {
-  alpha <- p * phi + 1e-7 # robust to 0
+  alpha <- p * phi + 1e-7
+  obs <- rgamma(length(p), alpha, 1)
+  obs <- obs / sum(obs)
+  obs
+}
+
+rdirichlet_linear <- function(p, theta, N) {
+  alpha <- p * theta * N + 1e-7
   obs <- rgamma(length(p), alpha, 1)
   obs <- obs / sum(obs)
   obs
@@ -12,8 +19,12 @@ rowMeans(out1)
 out2 <- sapply(1:10, function(i) rdirichlet(c(0.1, 0.1, 0.4, 0.4), 1))
 rowMeans(out2)
 
-rdirmultinom <- function(N, p, phi) {
-  dp <- rdirichlet(p, phi) # note bug in WHAM simulate(); should be done once per multinomial
+rdirmultinom <- function(N, p, phi, type = "saturating") {
+  if (type[[1]] != "saturating") {
+    dp <- rdirichlet_linear(p, phi, N)
+  } else {
+    dp <- rdirichlet(p, phi) # note bug in WHAM simulate(); should be done once per multinomial
+  }
   obs <- rmultinom(n = 1, prob = dp, size = N)
   obs
 }
@@ -32,8 +43,8 @@ sim_rdirmult <- function(N, phi) {
     )
   }
 }
-sim_rdirmult(100, 1)
-sim_rdirmult(100, 1000)
+sim_rdirmult(100, 0.1)
+sim_rdirmult(100, 100)
 
 get_neff <- function(N, phi) {
   (N + N * phi) / (N + phi)
@@ -53,3 +64,14 @@ sim_rdirmult(get_neff(300, 5), 1e9)
 sim_rdirmult(3000, 50)
 get_neff(3000, 50)
 sim_rdirmult(get_neff(3000, 50), 1e9)
+
+get_neff_linear <- function(N, theta) {
+  N * (theta / (1 + theta))
+}
+get_neff_linear(300, 1)
+
+# linear
+ns <- seq(1, 100, length.out = 100)
+plot(ns, get_neff_linear(ns, 100), type = "l", col = "red", ylab = "Neff", xlab = "N")
+lines(ns, get_neff_linear(ns, 1), type = "l", col = "blue")
+lines(ns, get_neff_linear(ns, 0.1), type = "l", col = "green")
